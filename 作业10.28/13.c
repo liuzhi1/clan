e5e7719b8e4c2ef8c5d32f2e6cbff46b40f83c50
liuzhi1 <1330183946@qq.com>
2020-10-29 13:23:31 +0800

    作业

---
+++ 作业10.28/13.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{
+	 char a1[]="china\n beijing";
+	 char a2[]="china\0 beijing";
+	  puts(a1); puts(a2);
+	  puts("WUHAN");
+}
---
+++ 作业10.28/14.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{	char a1[15],a2[15];
+	gets(a1);
+	scanf("%s",a2);
+	printf("a1=%s\n",a1);
+	printf("a2=%s\n",a2); 
+}
