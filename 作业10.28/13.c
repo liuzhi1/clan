70a1cfc6b96f26fdea2613399f5d453f357d6091
liuzhi1 <1330183946@qq.com>
2021-03-10 10:10:10 +0800

    1

--- 作业10.28/13.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main()
-{
-	 char a1[]="china\n beijing";
-	 char a2[]="china\0 beijing";
-	  puts(a1); puts(a2);
-	  puts("WUHAN");
-}
--- 作业10.28/14.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main()
-{	char a1[15],a2[15];
-	gets(a1);
-	scanf("%s",a2);
-	printf("a1=%s\n",a1);
-	printf("a2=%s\n",a2); 
-}
